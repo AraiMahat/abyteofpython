dd0a5cf74e0552ff6a42a3a0ac34ce1ccc4c356c
AraiMahat <oraemoroz@gmail.com>
2022-08-17 13:14:31 +0500

    first tutorial file

---
+++ str_format.py
@@ -0,0 +1,16 @@
+# PEP 3101 formatting #
+
+age = 26
+name = 'Swaroop'
+print('{0} {1}'.format(name,age))
+print('{0:.3}'.format(1/2))
+print('{0:_^15}'.format('hello'))
+print('{name}{book}'.format(name='Swaroop',book='Bible'))
+
+
+# Логические строки #
+
+i = 5
+print \
+(i)
+
